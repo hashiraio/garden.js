2c1209e2be83ab35b4d686e2e3a133f5649bf7ae
SaiAdithya3 <saiadithyakancharla@gmail.com>
2025-09-24 18:02:12 +0530

    done swapinprogress

--- packages/swap/src/lib/components/swapInProgress/SwapInProgress.tsx
+++ packages/swap/src/lib/components/swapInProgress/SwapInProgress.tsx
@@ -50,7 +50,7 @@ export const SwapInProgress = () => {
 
   return order ? (
     <div className="animate-fade-out flex flex-col gap-3 w-full ">
-      <div className="flex items-center justify-between p-1">
+      <div className="flex items-center justify-between p-1 px-2">
         <Typography size="h4" weight="medium">
           Swap in progress
         </Typography>
